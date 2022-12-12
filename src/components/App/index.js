fbabd7741e87c1778c6dc310e2576bd077268bbf
Oksana Nedilko <nedilko7b@gmail.com>
2022-12-12 21:21:25 +0200

    added unit tests for App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,7 +4,7 @@ function App() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
-        <Info user="plntry" />
+        <Info user="yurkovskiy" />
         <Info user="mplesha" />
       </div>
     );
